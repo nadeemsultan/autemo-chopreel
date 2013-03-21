2176cc3696026583d883345e817032fc847462f8
Nadeem Sultan <nadeemsultan1@hotmail.com>
2013-03-20 22:22:14 -0400

    Switched from CSS3 to jQuery for cross browser support

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 /*
- Autemo Chopreel 2.2
+ Autemo Chopreel 2.4
  by Nadeem Sultan   www.nadeemsultan.tk
 */
 
@@ -15,15 +15,15 @@ $(document).ready(function(){
 		div = $('#slider div');
 
 		rightArrow.click(function(){
-			image.animate({left: -980},300);
-			div.animate({left: -980},300);
+			image.stop().animate({left: -980},300);
+			div.stop().animate({left: -980},300);
 			$(this).css('opacity','0');
 			leftArrow.css('opacity','1');
 		});
 
 		leftArrow.click(function(){
-			image.animate({left: 0},300);
-			div.animate({left: 0},300);
+			image.stop().animate({left: 0},300);
+			div.stop().animate({left: 0},300);
 			$(this).css('opacity','0');
 			rightArrow.css('opacity','1');
 		});
@@ -34,14 +34,14 @@ $(document).ready(function(){
 		$(document).keydown(function(key) {
 	        switch(parseInt(key.which,10)) {
 				case 37:
-					image.animate({left: 0},300);
-					div.animate({left: 0},300);
+					image.stop().animate({left: 0},300);
+					div.stop().animate({left: 0},300);
 					leftArrow.css('opacity','0');
 					rightArrow.css('opacity','1');
 				break;
 				case 39:
-					image.animate({left: -980},300);
-					div.animate({left: -980},300);
+					image.stop().animate({left: -980},300);
+					div.stop().animate({left: -980},300);
 					rightArrow.css('opacity','0');
 					leftArrow.css('opacity','1');
 				break;
@@ -54,11 +54,102 @@ $(document).ready(function(){
 		// hover effects
 
 		$('#slider li a').hover(function(){
-			$(this).children('#slider div').css('transform','translate(0px,-115px)');
-			$(this).children('#slider div').css('opacity','.8');
+			/*$(this).children('#slider div').css('transform','translate(0px,-115px)');
+			$(this).children('#slider div').css('opacity','.8');*/
+			$(this).children('#slider div').stop().animate({top: -115}, 500, 'easeInOutQuart');
 		},
 			function(){
-				div.css('transform','translate(0px,0px)');
-				div.css('opacity','0');
+				/*div.css('transform','translate(0px,0px)');
+				div.css('opacity','0');*/
+				div.stop().animate({top: 0}, 500, 'easeInOutQuart');
 			});	
 });
+
+
+
+
+
+
+
+
+/*
+ * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ *
+ * Uses the built in easing capabilities added In jQuery 1.1
+ * to offer multiple easing options
+ *
+ * TERMS OF USE - jQuery Easing
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright Â© 2008 George McGinley Smith
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+*/
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	easeInOutQuart: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	}
+});
+
+/*
+ *
+ * TERMS OF USE - EASING EQUATIONS
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright Â© 2001 Robert Penner
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+ */
